30a5306e5e420d264a70414869b868ec9857b4ea
胡玮文 <huww98@outlook.com>
2020-11-12 00:16:16 +0800

    init

---
+++ src/.vuepress/config.js
@@ -0,0 +1,70 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'SMIL内部服务用户手册',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: '入门',
+        link: '/guide/',
+      },
+      {
+        text: '域名',
+        link: '/hostname/'
+      },
+      {
+        text: 'VuePress',
+        link: 'https://v1.vuepress.vuejs.org'
+      }
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: '入门',
+          collapsable: false,
+          children: [
+            '',
+            'using-vue',
+          ]
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
---
+++ src/index.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroImage: https://v1.vuepress.vuejs.org/hero.png
+tagline: 助你高效利用实验室资源
+actionText: 快速开始 →
+actionLink: /guide/
+features:
+- title: 域名解析
+  details: 忘记IP地址，使用简短的域名访问任何服务器
+- title: 统一认证
+  details: 用同样的凭据登录任何服务
+- title: 分布式存储
+  details: 在任何地方访问任何文件
+footer: Made by SMIL Infrastructure team with ❤️
+---
